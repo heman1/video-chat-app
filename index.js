5ccd94d3e699ee7ee13aa4f28be117379f025ec0
hero-007 <akhil1998tiwari@gmail.com>
2020-02-29 18:43:57 +0530

    include ejs

--- index.js
+++ index.js
@@ -4,12 +4,54 @@ const jwt = require('jsonwebtoken');
 
 //app setup
 var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+// user-model
+var User = require("./models/user-model");
+
+// connect to mongoDB database 
+mongoose.connect("mongodb://localhost:27017/video-chat",{useNewUrlParser:true, useUnifiedTopology:true});
+
+app.use(express.static(__dirname+"/public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(
+  require("express-session")({
+    secret: "video chat app",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+
+// initializing passport 
+app.use(passport.initialize());
+app.use(passport.session());
+// serialize and deserialize data from the session using the methods provided by passport 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// put the loggedIn user info inside res.locals, so that it can be used by all the routes 
+app.use(function(req,res,next){
+  res.locals.currentUser = req.user;
+  next();
+});
 
 //static files
-app.use(express.static('public'));
-var server = app.listen(4001, function() {
-	console.log('server listening at port 4001');
-});
+// app.use(express.static('public'));
+// var server = app.listen(4001, function() {
+// 	console.log('server listening at port 4001');
+// });
+
+var server = app.listen(3000,function(req,res){
+  console.log("Server Started Successfully......");
+}); 
+
 //socket.io setup
 var io = socket(server);
 //check when a client is connected to the socket
@@ -126,8 +168,3 @@ getUserMedia({ video: true, audio: true }, function (err, stream) {
   })
 })
 */
-
-// running app on local host
-app.listen(3000, function(req, res) {
-	console.log('Server started successfully.....');
-});
